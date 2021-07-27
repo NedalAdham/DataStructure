e145d2a2774911ff07f0cf9d61c54e1906c6b35c
NedalAdham <75937336+NedalAdham@users.noreply.github.com>
2021-07-27 18:40:03 +0200

    Add files via upload

---
+++ BFS.cpp
@@ -0,0 +1,39 @@
+void bfsprint(vector<vector<int>> v , int sv)
+{
+    int n = v.size();
+    vector<bool>visited(n, false);
+    queue <int> q ;
+
+    q.push(sv);
+    visited[sv] = true;
+    while (!q.empty())
+    {
+        int cv = q.front();
+        q.pop();
+        cout <<cv <<endl;
+
+        for (int i = 0; i <n ; ++i)
+        {
+         if(v[cv][i] && !visited[i] )
+         {
+             q.push(i) ;
+             visited[i] = true ;
+         }
+        }
+    }
+}
+int main()
+{
+    int n,e ;
+    cin >> n >> e ;
+    vector<vector<int> > matrix (n,vector<int>(n,0));
+    for (int i = 0; i < e ; ++i)
+    {
+        int fv , sv ;
+        cin >> fv >> sv ;
+        matrix[fv][sv]=1;
+        matrix[sv][fv]=1;
+    }
+    vector <bool>visited(n,false);
+    bfsprint( matrix , 0);
+}
